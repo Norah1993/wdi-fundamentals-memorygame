74c8bd897461c9bc3dc9bdeac8135d0d401ce8f2
Norah Hassan <angel0o02010@windowslive.com>
2019-04-19 19:33:34 +0300

    Create an array cards

--- memory_game/Main.js
+++ memory_game/Main.js
@@ -1,7 +1,6 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
+alert('Hello, friends.');
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay =[]
