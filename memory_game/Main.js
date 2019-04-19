e469bb847238ba6c451e0533a21545fe01fb4e06
Norah Hassan <angel0o02010@windowslive.com>
2019-04-19 20:18:32 +0300

    use push on Main.js file

--- memory_game/Main.js
+++ memory_game/Main.js
@@ -1,6 +1,17 @@
 console.log("Up and running!");
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
-alert('Hello, friends.');
 var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay =[]
+var cardsInPlay =[];
+var cardOne = "queen" ;
+cards.push('cardOne');
+console.log("User flipped queen");
+var cardTwo = "king" ;
+cards.push('cardTwo');
+console.log("User flipped king");
+	if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+	alert("You found a match!");
+}
+    else {
+	alert("Sorry try again");
+}
+}
